cecb7e71e601892fe4e77976c0528d553905bdff
Shaharia Ahmed <adeptshaharia@gmail.com>
2022-07-20 01:35:31 +0600

    pull2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Slider from "./pages/Home/Slider";
 
 
 function App() {
   return (
     <div className="">
       <h2>Ticket Kato</h2>
-     
+       <Slider></Slider>
       
     </div>
   );
