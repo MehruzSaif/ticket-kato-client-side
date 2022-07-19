040b3d8a9fa704fc7b7ad0ba984843cc5ff562be
Shaharia Ahmed <adeptshaharia@gmail.com>
2022-07-19 14:24:01 +0600

    slider-code

--- src/pages/Home/Slider.js
+++ src/pages/Home/Slider.js
@@ -1,52 +1,74 @@
 import React from 'react';
+import "./Slider.css"
 import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
+import  img1 from'./images/1649696504.jpg';
+import img2 from './images/istockphoto-135327019-612x612.jpg'
+import img3 from'./images/1649696504.jpg'
+import img4 from'./images/Mayanmi.jpg'
+import img5 from'./images/train.jpg'
 
 const Slider = () => {
     return (
+        <div className='owl-slider'>
+            <div className='slidr-dev'>
         <OwlCarousel className='owl-theme'
-          items='3'
+          items='4'
           autoplay
           dots
          loop margin={10} nav>
     <div class='item'>
-        <h4>1</h4>
+        <img src={img1} alt=""  />
+        <h2>Find Biman Bangladesh Airlines Ticket and Schedule on ShareTrip</h2>
     </div>
     <div class='item'>
-        <h4>2</h4>
+    <img src={img2} alt=""  />
+    <h2>Find Biman Bangladesh Airlines Ticket and Schedule on ShareTrip</h2>
     </div>
     <div class='item'>
-        <h4>3</h4>
+    <img src={img3} alt=""  />
+    <h2>Find Biman Bangladesh Airlines Ticket and Schedule on ShareTrip</h2>
     </div>
     <div class='item'>
-        <h4>4</h4>
+    <img src={img4} alt=""  />
+    <h2>Find Biman Bangladesh Airlines Ticket and Schedule on ShareTrip</h2>
     </div>
     <div class='item'>
-        <h4>5</h4>
+    <img src={img5} alt=""  />
+    <h2>Find Biman Bangladesh Airlines Ticket and Schedule on ShareTrip</h2>
     </div>
     <div class='item'>
-        <h4>6</h4>
+    <img src={img1} alt=""  />
+    <h2>Find Biman Bangladesh Airlines Ticket and Schedule on ShareTrip</h2>
     </div>
     <div class='item'>
-        <h4>7</h4>
+    <img src={img2} alt=""  />
+    <h2>Find Biman Bangladesh Airlines Ticket and Schedule on ShareTrip</h2>
     </div>
     <div class='item'>
-        <h4>8</h4>
+    <img src={img3} alt=""  />
+    <h2>Find Biman Bangladesh Airlines Ticket and Schedule on ShareTrip</h2>
     </div>
     <div class='item'>
-        <h4>9</h4>
+    <img src={img4} alt=""  />
+    <h2>Find Biman Bangladesh Airlines Ticket and Schedule on ShareTrip</h2>
     </div>
     <div class='item'>
-        <h4>10</h4>
+    <img src={img5} alt=""  />
+    <h2>Find Biman Bangladesh Airlines Ticket and Schedule on ShareTrip</h2>
     </div>
     <div class='item'>
-        <h4>11</h4>
+    <img src={img1} alt=""  />
+    <h2>Find Biman Bangladesh Airlines Ticket and Schedule on ShareTrip</h2>
     </div>
     <div class='item'>
-        <h4>12</h4>
+    <img src={img2} alt=""  />
+    <h2>Find Biman Bangladesh Airlines Ticket and Schedule on ShareTrip</h2>
     </div>
 </OwlCarousel>
+</div>
+</div>
     );
 };
 
