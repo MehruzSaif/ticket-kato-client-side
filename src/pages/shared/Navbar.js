80867bab2029616b01694862815a834cf7c0863b
Mehedi Hassan <rehan51322@gmail.com>
2022-07-21 18:57:26 +0600

    navbar bug fix

--- src/pages/shared/Navbar.js
+++ src/pages/shared/Navbar.js
@@ -5,27 +5,30 @@ const Navbar = () => {
     <div>
       <nav class="navbar navbar-expand-lg navbar-light bg-white">
         <div class="container-fluid ">
-            <a class="navbar-brand" href="#">
-              <img
-                href="#"
-                src="https://i.ibb.co/JdDqrdk/ticket-Kato-2.png"
-                alt=""
-                width="125"
-                height="40"
-              />
-            </a>
+          <a class="navbar-brand" href="#">
+            <img
+              href="#"
+              src="https://i.ibb.co/JdDqrdk/ticket-Kato-2.png"
+              alt=""
+              width="125"
+              height="40"
+            />
+          </a>
           <button
             class="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="#navbarTogglerDemo02"
-            aria-controls="navbarTogglerDemo02"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <div
+            class="collapse navbar-collapse justify-content-end"
+            id="navbarSupportedContent"
+          >
             {/* <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">
