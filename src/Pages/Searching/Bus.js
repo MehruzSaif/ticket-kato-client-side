beff863bff3bacfa813517b3fcb84cfb680cf6fd
wakeupzaman909 <hadiuzzaman909@outlook.com>
2022-08-05 17:20:21 +0600

    desiabled previous date

--- src/Pages/Searching/Bus.js
+++ src/Pages/Searching/Bus.js
@@ -7,17 +7,19 @@ import './Search.css';
 import { DatePicker } from 'antd';
 import "antd/dist/antd.css";
 import axios from 'axios';
-import { Link,Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import moment from 'moment';
+
 
 const Bus = () => {
     const [districts, setDistricts] = useState([]);
     const [text, setText] = useState('');
     const [suggestions, setSuggestions] = useState([])
-
+    console.log(text)
     const [districts2, setDistricts2] = useState([]);
     const [text2, setText2] = useState('');
     const [suggestions2, setSuggestions2] = useState([])
-
+    console.log(text2);
     useEffect(() => {
         const loadDistricts = async () => {
             const response = await axios.get('https://bdapis.herokuapp.com/api/v1.1/districts');
@@ -50,6 +52,7 @@ const Bus = () => {
     }
 
     const onSuggestionHandler = (text) => {
+        console.log(text)
         setText(text)
         setSuggestions([])
     }
@@ -68,69 +71,77 @@ const Bus = () => {
     }
 
     const onSuggestionHandler2 = (text2) => {
+        console.log(text2)
         setText2(text2)
         setSuggestions2([])
     }
+    const disabledDate = (current) => {
+        // Can not select days before today and today
+        return current && current < moment().add(-1, 'days').endOf('day');
+      };
+
     return (
         <div>
             <h6 className='text-center mt-3 flight-info'>Buy Bus Tickets with Best Offers and Discounts !</h6>
-        <div className='d-flex justify-content-evenly text-center search-content mt-0'>
-        <div>
-            <label for="From" class="form-label mt-2 fs-4">From</label>
-            <input type="text"
-                onChange={e => onChangeHandler(e.target.value)}
-                value={text}
-                onBlur={() => {
-                    setTimeout(() => {
-                        setSuggestions([])
-                    }, 100);
-                }}
-                class="form-control"
-                name='From'
-                placeholder='        Departure Place'
-                required />
-        </div>
+            <div className='d-flex justify-content-evenly text-center search-content mt-0'>
+                <div>
+                    <label for="From" class="form-label mt-2 fs-4">From</label>
+                    <input type="text"
+                        onChange={e => onChangeHandler(e.target.value)}
+                        value={text}
+                        onBlur={() => {
+                            setTimeout(() => {
+                                setSuggestions([])
+                            }, 100);
+                        }}
+                        class="form-control"
+                        name='From'
+                        placeholder='        Departure Place'
+                        required />
+                </div>
 
-        <div>
-            <label for="To" class="form-label mt-2 fs-4">To</label>
-            <input type="text"
-                onChange={e => onChangeHandler2(e.target.value)}
-                value={text2}
-                onBlur={() => {
-                    setTimeout(() => {
-                        setSuggestions2([])
-                    }, 100);
-                }}
-                class="form-control"
-                name='To'
-                placeholder='          Arrival Place'
-                required />
-        </div>
-        <div>
+                <div>
+                    <label for="To" class="form-label mt-2 fs-4">To</label>
+                    <input type="text"
+                        onChange={e => onChangeHandler2(e.target.value)}
+                        value={text2}
+                        onBlur={() => {
+                            setTimeout(() => {
+                                setSuggestions2([])
+                            }, 100);
+                        }}
+                        class="form-control"
+                        name='To'
+                        placeholder='          Arrival Place'
+                        required />
+                </div>
+                <div>
             <label for="date" class="form-label mt-2 fs-4">Travel Date</label><br />
-            <DatePicker className='departing' />
+            <DatePicker className='departing'
+                disabledDate={disabledDate} />
         </div>
         <div>
             <label for="date" class="form-label mt-2 fs-4">Return Date</label><br />
-            <DatePicker className='returning' />
+            <DatePicker className='returning' 
+            disabledDate={disabledDate}/>
         </div>
-    </div>
-    <div className='d-flex justify-content-center mt-3'>
-            <button className='search-button'>Search Buses</button>
-        </div>
-        <div className='from-text' >{suggestions && suggestions.slice(0,4).map((suggestion, i) =>
-            <div
-                onClick={() => onSuggestionHandler(suggestion.district)}
-                className='suggestion' key={i}>{suggestion.district}</div>
-        )}</div>
-        <div className='from-text2' >{suggestions2 && suggestions2.slice(0,4).map((suggestion2, i) =>
-            <div
-                onClick={() => onSuggestionHandler2(suggestion2.district)}
-                className='suggestion' key={i}>{suggestion2.district}</div>
-        )}</div>
+            </div>
+            <div className='d-flex justify-content-center mt-3'>
+                <button className='search-button'>Search Buses</button>
+            </div>
+            <div className='from-text' >{suggestions && suggestions.map((suggestion, i) =>
+                <div
+                    onClick={() => onSuggestionHandler(suggestion.district)}
+                    className='suggestion' key={i}>{suggestion.district}</div>
+            )}</div>
+            <div className='from-text2' >{suggestions2 && suggestions2.map((suggestion2, i) =>
+                <div
+                    onClick={() => onSuggestionHandler2(suggestion2.district)}
+                    className='suggestion' key={i}>{suggestion2.district}</div>
+            )}</div>
 
 
-    </div>
+        </div>
     );
 };
 
