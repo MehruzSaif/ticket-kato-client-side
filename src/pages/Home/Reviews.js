1d911c6d7fcf3a7a08323ce9ad1204db8da9f623
Nargis21 <nargisakther1933@gmail.com>
2022-08-05 20:13:29 +0600

    update

--- src/pages/Home/Reviews.js
+++ src/pages/Home/Reviews.js
@@ -15,7 +15,7 @@ import Review from './Review';
 const Reviews = () => {
     const [reviews, setReviews] = useState([])
     useEffect(() => {
-        fetch('http://localhost:5000/reviews')
+        fetch('https://intense-dawn-70089.herokuapp.com/reviews')
             .then(res => res.json())
             .then(data => setReviews(data))
     }, [])
