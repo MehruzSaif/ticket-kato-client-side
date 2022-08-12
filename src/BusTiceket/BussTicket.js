332632e802f2d68d17e9ed1626866e8999bff34c
Daniel Newaz <danielnwz420@gmail.com>
2022-08-12 16:21:37 +0600

    each bus

--- src/BusTiceket/BussTicket.js
+++ src/BusTiceket/BussTicket.js
@@ -4,13 +4,91 @@ import Search from '../pages/Searching/Search';
 import './BussTiceket.css'
 
 const BussTicket = () => {
+
   const [bus,setBus]=useState([]);
+  const [busdata,setBusdata]=useState([]);
+  const array = [...busdata]
+  console.log(array)
+
   useEffect(()=>{
     fetch('http://localhost:5000/busInfo')
     .then(response => response.json())
     .then(data =>setBus(data))
   },[])
+    const [hyundai, setHyundai] =useState(true);
+    const [hino, setHino] =useState(true);
+    const [ak1j, setAk1j] =useState(true);
+    const [scania, setScania] =useState(true);
 
+    const handleChange=(data) =>{
+      if(data==" Hyundai Universe")
+      {
+        if(hyundai==true){
+          console.log(data,"our value")
+          const result = bus.filter(word => 
+            { return  word.bus_class === data
+             console.log(word.bus_class)  
+          }
+            );
+            setBus(result)
+            // setHino(false)
+            // setAk1j(false)
+            // setScania(false)
+            // console.log(result)
+            // setBus (array);
+        }
+  setHyundai(!hyundai)
+      }
+      if(data==" Hino 1J Pluss")
+      {
+        if(hino==true){
+          console.log(data,"our value")
+          const result = bus.filter(word => 
+            { return  word.bus_class===data
+               
+          }
+            );
+            setBus(result)
+            // setHyundai(false)
+            // setAk1j(false)
+            // setScania(false)
+        }
+        setHino(!hino)
+      }
+      if(data=="AK1J Super Plus")
+      {
+        if(ak1j==true){
+          console.log(data,"our value")
+          const result = bus.filter(word => 
+            { return  word.bus_class===data
+               
+          }
+            );
+            setBus(result)
+            // setHyundai(false)
+            // setHino(false)
+            // setScania(false)
+        }
+        setAk1j(!ak1j)
+        // setBus(!result)
+      }
+      if(data==" Scania")
+      {
+        if(scania==true){
+          console.log(data,"our value")
+          const result = bus.filter(word => 
+            { return  word.bus_class===data
+               
+          }
+            );
+            setBus(result)
+            // setHyundai(false)
+            // setHino(false)
+            // setAk1j(false)
+        }
+        setScania(!scania)
+      }
+    }
     return (
         <div className='busTiceket'>
           <h1>hi this is md</h1>
@@ -18,15 +96,15 @@ const BussTicket = () => {
                 <Search></Search>
               <div className='BusTicket'>
                 <div className='filter'>
-                      <div>
-                        <p>Quick Filter</p>
+                <div>
+                        <p>Buss Class</p>
                           <ul>
                             
-                             <li><input type="checkbox" value={'First Class'} />  First Class</li>
-                             <li><input type="checkbox" value={'First Class'} />  Second Class</li>
-                             <li><input type="checkbox" value={'First Class'} />  AC</li>
-                             <li><input type="checkbox" value={'First Class'} />  Non AC</li>
-                             <li><input type="checkbox" value={'First Class'} />  Sleeper</li>
+                             <li><input type="checkbox" value={'Hyundai Universe'} onChange={()=>handleChange(" Hyundai Universe")} /> Hyundai Universe</li>
+                             <li><input type="checkbox" value={'Hino 1J Pluss'} onChange={()=>handleChange(" Hino 1J Pluss")}/>  Hino 1J Pluss</li>
+                             <li><input type="checkbox" value={'AK1J Super Plus'} onChange={()=>handleChange("AK1J Super Plus")}/>  AK1J Super Plus</li>
+                             {/* <li><input type="checkbox" value={'Hyundai Universe'} onChange={()=>handleChange("Hyundai Universe")}/>  Hyundai Universe</li> */}
+                             <li><input type="checkbox" value={'Scania'} onChange={()=>handleChange(" Scania")}/>  Scania</li>
                           </ul>
                       </div>
                       <div>
@@ -63,7 +141,7 @@ const BussTicket = () => {
                               <div>
                               <p>Arrival Time: {item.arrival_time}</p>
                               <p>Available Seats: {item.available_seats}</p>
-                              <button className="btn bg-danger">Booking</button>
+                              <button className="btn bg-success m-2">Booking</button>
                               </div>
                         </div>)
                     }
