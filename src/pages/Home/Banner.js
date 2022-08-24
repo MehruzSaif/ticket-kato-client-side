c3363048a62028fd67be40c8027f43d6cfcf2f1d
Your Name <adeptshaharia@gmail.com>
2022-08-24 11:09:52 -0700

    type-writer

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -2,13 +2,14 @@ import React from "react";
 import { useTypewriter } from "react-simple-typewriter";
 // import 'react-simple-typewriter/dist/'; 
 import "./Banner.css";
+import Typed from 'react-typed';
 
 const Banner = () => {
-  const { text } = useTypewriter({
-    words: ["Bus", "Train", "Launch", "Domestic Airline"],
-    loop: Infinity,
-    onLoopDone: () => console.log(`loop completed after 3 runs.`),
-  });
+  // const { text } = useTypewriter({
+  //   words: ["Bus", "Train", "Launch", "Domestic Airline"],
+  //   loop: Infinity,
+  //   onLoopDone: () => console.log(`loop completed after 3 runs.`),
+  // });
 
 
 
@@ -16,11 +17,29 @@ const Banner = () => {
         <div className='banner'>
             <div className='main-div-banner'>
                 <div>
-                    <h1>Book Your <br /> Tickets Fors
-                        <h2 class='typewriter'> {text}</h2>
-                    </h1>
-
-                    <p className='text-center'>Safe, Secure, Reliable Ticketing!</p>
+                        <div>
+                          <h1>
+                <Typed
+                    strings={['Book Your Tickets Fors']}
+                    typeSpeed={40}
+                />
+                <br/>
+ 
+                <Typed
+                strings={[
+                    'Bus',
+                    'Train',
+                    'Launch',
+                  'Domestic Airline']}
+                    typeSpeed={40}
+                    backSpeed={50}
+                    attr="placeholder"
+                    loop >
+                    <input className="text-center mt-2" type="text"/>
+                </Typed>
+                </h1>
+                <p className='text-center'>Safe, Secure, Reliable Ticketing!</p>
+              </div>
                 </div>
             </div>
   </div>
